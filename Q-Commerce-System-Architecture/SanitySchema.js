c796b7efd4daa3205f73ee77fba9f7817fd8347c
unknown <nizakatalilaghari7@gmail.com>
2025-01-17 11:54:31 +0500

    Q-Commerce System Architecture

---
+++ Q-Commerce-System-Architecture/SanitySchema.js
@@ -0,0 +1,24 @@
+// GET /products
+
+
+{
+  "status": "success",
+  "data": [
+    {
+      "id": "101",
+      "name": "Veggie Burger",
+      "price": 5.99,
+      "category": "Food",
+      "description": "A delicious veggie burger with fresh ingredients.",
+      "image_url": "XY"
+    },
+    {
+      "id": "102",
+      "name": "Cheese Pizza",
+      "price": 7.99,
+      "category": "Food",
+      "description": "A cheesy delight with a perfect crust.",
+      "image_url": ""
+    }
+  ]
+}
